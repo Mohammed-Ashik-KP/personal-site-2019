556316e55be6333d97685cde4acf491ba49ff393
Aashiq-Otp <Aashiqotp.as@gmail.com>
2020-09-20 13:22:09 +0530

    Updates

---
+++ precache-manifest.3cef10de28e30b80f8a458e0e3fddbda.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f5230ac42a3d1b3c26cf7ef613a808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aacb715d4d1e260c6160",
+    "url": "/static/css/2.eea0d0d8.chunk.css"
+  },
+  {
+    "revision": "e5b1693c604988de8f32",
+    "url": "/static/css/main.c58b0633.chunk.css"
+  },
+  {
+    "revision": "aacb715d4d1e260c6160",
+    "url": "/static/js/2.be98f618.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.be98f618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b1693c604988de8f32",
+    "url": "/static/js/main.9d41cda2.chunk.js"
+  },
+  {
+    "revision": "55a6e25ef11153be2952",
+    "url": "/static/js/runtime-main.6950a9b6.js"
+  },
+  {
+    "revision": "cca9e7ac7ceb5bd74b1d09713a9a14bd",
+    "url": "/static/media/ashiq.cca9e7ac.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d6af91a226883839f448b4bba0fa2e7b",
+    "url": "/static/media/smui.d6af91a2.jpg"
+  },
+  {
+    "revision": "f00d04db068c2ac946d6c5a3b571bcd6",
+    "url": "/static/media/workingui.f00d04db.png"
+  }
+]);
